9e1b8232708d50006bc461d41283dc62e5bcb060
zegadb <zeygammehtiev@gmail.com>
2022-11-21 12:00:26 +0400

    done

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const convert = async (from, to) => {
     return Object.values(data.rates)[0]
 }
 
+function setRight () { rightInput.value = format(+leftInput.value * +leftValue.textContent) }
+function setLeft () { leftInput.value = format(+rightInput.value * +rightValue.textContent) }
+
 let from = 'RUB', to = 'USD'
 const leftFrom = document.querySelector('.left .from')
 const leftTo = document.querySelector('.left .to')
@@ -15,14 +18,17 @@ const rightTo = document.querySelector('.right .to')
 const rightValue = document.querySelector('.right .value')
 const rightInput = document.querySelector('.right input')
 const allRightCur = document.querySelectorAll('.right .changeCurrency li')
-function setAll() {
-    convert(from, to).then(ratio => {
+
+function setAll(side) {
+    convert(from, to, side).then(ratio => {
         leftFrom.textContent = from
         leftTo.textContent = to
-        leftValue.textContent = ratio
+        leftValue.textContent = ratio.toFixed(4)
         rightFrom.textContent = to
         rightTo.textContent = from
-        rightValue.textContent = 1 / ratio
+        rightValue.textContent = (1 / ratio).toFixed(4)
+        if (side == false) setRight()
+        else if (side == true) setLeft()
     })
 }
 setAll()
@@ -32,7 +38,7 @@ allLeftCur.forEach(item =>{
         allLeftCur.forEach(item => item.classList.remove("selected"))
         event.target.classList.add("selected")
         from = event.target.textContent
-        setAll()
+        setAll(false)
     })
 })
 
@@ -41,6 +47,28 @@ allRightCur.forEach(item =>{
         allRightCur.forEach(item => item.classList.remove("selected"))
         event.target.classList.add("selected")
         to = event.target.textContent
-        setAll()
+        setAll(true)
     })
 })
+
+leftInput.addEventListener('input', (event) => {
+    event.target.value = event.target.value.replace(/[^0-9.,]/g, '').replace(/,/g, '.').replace(/(\..*)\./g, '$1')
+    setRight()
+    event.target.value = format(event.target.value)
+})
+rightInput.addEventListener('input', (event) => {
+    event.target.value = event.target.value.replace(/[^0-9.,]/g, '').replace(/,/g, '.').replace(/(\..*)\./g, '$1')    
+    setLeft()
+    event.target.value = format(event.target.value)    
+})
+
+const onlyZeros = (str) => { return str.toString().trim() == 0 }
+function format (number) {
+    string = number.toString()
+    if (string.indexOf('.') == -1) return string.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ')
+    let parts = string.split('.')
+    parts[0] = parts[0].replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ')
+    if (onlyZeros(parts[1])) parts[1] = ''
+    else parts[1] = parts[1].substring(0, 4)
+    return parts[0] + '.' + parts[1]
+}
